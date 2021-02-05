66423aa0b89c07e0f1e2a8ee5cef02b4a0749e6c
khatskelevich <khatskelevich@proitr.ru>
2021-02-05 15:02:03 +0500

    Add empty Game controller.

---
+++ app/Http/Controllers/Game.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class Game extends Controller
+{
+
+    public function __construct(Request $request)
+    {
+    }
+
+    public function getHistory()
+    {
+    }
+}
