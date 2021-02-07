000cfeb08ecdef6f8bd36370cca76c5b97944fde
khatskelevich <khatskelevich@proitr.ru>
2021-02-07 18:00:18 +0500

    Add some methods in Game controller

--- app/Http/Controllers/Game.php
+++ app/Http/Controllers/Game.php
@@ -5,15 +5,30 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class Game extends Controller
 {
 
+    private $user_id;
+
     public function __construct(Request $request)
     {
+        $this->user_id = Auth::id();//$request->user;
     }
+
+    public function saveStatistic(Request $request)
+    {
+
+    }
+
 
     public function getHistory()
     {
+        return DB::table('games')
+            ->where('user_id', '=',  $this->user_id)
+            ->orderBy('created_at', 'desc')
+            ->get();
     }
 }
