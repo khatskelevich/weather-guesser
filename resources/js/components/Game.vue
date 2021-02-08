0654dff5e53563794a7e1bd94fdb1169bd6ffbee
khatskelevich <khatskelevich@proitr.ru>
2021-02-09 02:07:50 +0500

    Temporary layout for main game window. Add loading from api.

--- resources/js/components/Game.vue
+++ resources/js/components/Game.vue
@@ -1,12 +1,42 @@
 <template>
-  <div class="container">
-    <h3>Игра</h3>
+  <div>
+    <div v-if="loaded">
+      <div :id=1 class="button" @click="chooseCity(1)">{{ cities[0].temperature }}<br>{{ cities[0].city }}</div>
+      <div :id=2 class="button" @click="chooseCity(2)">
+        {{ cities[1]['temperature'] }}<br>{{ cities[1].city + ', ' + cities[1].country }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Game"
+  data() {
+    return {
+      cities: [],
+      loaded: false
+    }
+  },
+  created() {
+    this.getNewCities();
+  },
+  methods: {
+    getNewCities() {
+      axios.get('api/cities')
+          .then((response) => {
+            this.cities = response.data;
+            this.loaded = true;
+            console.log(this.cities);
+          })
+          .catch(function (resp) {
+            console.log(resp);
+            alert("Не удалось загрузить города");
+          });
+    },
+    chooseCity(id) {
+      this.getNewCities();
+    }
+  },
 }
 </script>
 
