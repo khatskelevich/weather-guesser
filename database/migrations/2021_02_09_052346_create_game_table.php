9b8badc07f922885e5d9be328dfe210dc4b34708
khatskelevich <khatskelevich@proitr.ru>
2021-02-09 12:12:58 +0500

    Add migration for games table

---
+++ database/migrations/2021_02_09_052346_create_game_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGameTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->timestamp('created_at')->nullable(false);
+            $table->integer('user_id')->nullable(false);
+            $table->jsonb('game_stat');
+            $table->boolean('guessed');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
