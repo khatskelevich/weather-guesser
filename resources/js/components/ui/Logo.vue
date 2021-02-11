12af2b4ef62e3f526cc6acc628ce9de079d3be5b
khatskelevich <khatskelevich@proitr.ru>
2021-02-11 19:35:32 +0500

    Add svg logo.

---
+++ resources/js/components/ui/Logo.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+    <g>
+      <rect fill="#fff" id="canvas_background" height="52" width="52" y="-1" x="-1"/>
+      <g display="none" overflow="visible" y="0" x="0" height="100%" width="100%" id="canvasGrid">
+        <rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/>
+      </g>
+    </g>
+    <g>
+      <g id="svg_6"/>
+      <text xml:space="preserve" text-anchor="start" font-size="24" font-weight="bold" id="svg_9" y="27.5" x="9.5" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#000000">W</text>
+      <text xml:space="preserve" text-anchor="start" font-size="24" font-weight="bold" id="svg_10" y="36.5" x="25" fill-opacity="null" stroke-opacity="null" stroke-width="0" stroke="#000" fill="#000000">G</text>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "Logo"
+}
+</script>
+
+<style scoped>
+
+</style>
